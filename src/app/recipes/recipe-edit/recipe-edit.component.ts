331a29cd1592c6a14efd42936f18f4f652845840
Michael Truong <truong.michael.2013@gmail.com>
2023-03-04 14:41:52 -0500

    Add recipe edit router component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+
+  id: number;
+  editMode = false;
 
   ngOnInit(): void {
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id = +params['id']
+        // If the 'id' is missing from params, then it should navigate to creating a new recipe as opposed to editing an existing one 
+        this.editMode = (params['id'] != null);
+        console.log(this.editMode)
+      }
+    )
   }
 
 }
